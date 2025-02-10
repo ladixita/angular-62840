d201ede023a88fee48db8665586a3a3e977e8ab3
leidy <101063765+luribeCT@users.noreply.github.com>
2025-02-10 02:57:55 -0500

    feat: realizando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { Alumno } from './interfaces/Alumno.Interface';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: '**', //representa todas las rutas que no existen, como el default
+    redirectTo: 'auth/login',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
